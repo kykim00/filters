cbcffda5eacf4f444f497bbee13bb73a1576ba5f
kykim00 <jeongsaeki@likelion.org>
2022-05-11 16:41:52 +0900

    refactor: react-query 로 데이터 캐시

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { QueryClientProvider } from "react-query";
 import MainPage from "./pages";
+import { getClient } from "./queryClient";
 
 function App() {
-  return <MainPage />;
+  const queryClient = getClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainPage />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,16 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import ProductList from "../components/products/ProductList";
 import { Products, QueryParams } from "../types";
 import { getProductList } from "../utils/api";
 
 const MainPage = () => {
-  const [data, setData] = useState<Products>();
   const [params, setParams] = useState<QueryParams>({});
-  const getData = async () => {
-    const response = await getProductList(params);
-    setData(response);
-    return response;
-  };
-  console.log(data);
-  useEffect(() => {
-    getData();
-  }, []);
+
+  const { data } = useQuery(["products", params], () => getProductList(params));
+
   if (!data) return <div>Loading...</div>;
+  console.log(data);
   return (
     <div>
       <h1>Hello World</h1>
